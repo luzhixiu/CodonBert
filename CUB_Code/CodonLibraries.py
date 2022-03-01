9caec326061f4eb2bb66a7852c138850a10a03e0
lu <luzhixiu1996@gmail.com>
2022-03-01 03:51:27 -0500

    was able to pre-train on sequence files somewhat, need better tuning

---
+++ CUB_Code/CodonLibraries.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.stats as ss
+import math
+import random as rd
+
+def loadSequence(sequence):
+    startCodon="ATG"
+    stopCodonList=["TAG","TAA","TGA"]
+    codonList=[]
+    i=0
+    while(i<len(sequence)):
+        codon=sequence[i:i+3]
+        if len(codon)==3:
+            codonList.append(codon)
+        i+=3
+    actualCodonList=[]
+    started=False
+    for codon in codonList:
+        if codon in stopCodonList:
+            break
+        if started:
+            actualCodonList.append(codon)
+        if codon==startCodon:
+            started=True
+    codonList=actualCodonList
+   # print "codon readed successful, the number of codon in this sequence is %d"%(len(codonList))
+    return codonList
